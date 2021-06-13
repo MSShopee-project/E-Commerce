d7766b46ec1eb6cf88a9f3a54c73161c2eb60547
Sarfraz Hussain <sarfaraz.hussain856@gmail.com>
2021-06-14 00:38:18 +0530

    added link in navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
-=======
 import React from 'react';
 
 import './App.css';
 import Navbar from './Components/Navbar';
->>>>>>> 2512a66c3257fad0f0a47bb390615749f2817056
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <nav>
         <ul>
           <li>home</li>
@@ -21,9 +17,7 @@ function App() {
           <li>Hospitality</li>
         </ul>
       </nav>
-=======
       <Navbar/>
->>>>>>> 2512a66c3257fad0f0a47bb390615749f2817056
     </div>
   );
 }
