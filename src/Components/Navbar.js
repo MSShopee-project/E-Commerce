2512a66c3257fad0f0a47bb390615749f2817056
shahanawajalam <67696792+shahanawajalam@users.noreply.github.com>
2021-06-13 23:03:27 +0530

    navbar
    created Functional navbar Component in component folder

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Navbar.css';
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Project</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
