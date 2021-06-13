ca371981379d9b2736688ddb5332c946093b3421
Sarfraz Hussain <sarfaraz.hussain856@gmail.com>
2021-06-13 23:37:24 +0530

    li added in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-     <nav>
-       <ul>
-         <li>home</li>
-         <li>Contact</li>
-         <li>About</li>
-         <li>Project</li>
-       </ul>
-     </nav>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Project</li>
+          <li>Hospitality</li>
+        </ul>
+      </nav>
     </div>
   );
 }
